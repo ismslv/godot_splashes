f019c8171f3a0cfa2251face885e25f2f8729699
smslv <7soul.off@gmail.com>
2023-09-30 21:03:56 +0200

    correct_delays

--- scripts/splash.gd
+++ scripts/splash.gd
@@ -3,12 +3,14 @@ extends Node3D
 @export var model2: RigidBody3D
 @export var labels: Control
 @export var labelsAnimator: AnimationPlayer
+@export var timeoutFirst = 1.0
+@export var timeoutSecond = 1.0
 
 func _ready():
 	labels.hide()
-	await get_tree().create_timer(1.0).timeout
+	await get_tree().create_timer(timeoutFirst).timeout
 	labels.show()
-	await get_tree().create_timer(1.0).timeout
+	await get_tree().create_timer(timeoutSecond).timeout
 	model2.freeze = false
-	await get_tree().create_timer(1.5).timeout
+	await get_tree().create_timer(timeoutSecond + 0.5).timeout
 	labelsAnimator.play("labels_animation")
